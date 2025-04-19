297a9b2cc2d6604fcda2a04880b6fdfa9592c14a
RKS File <ronitsahu428@gmail.com>
2025-04-19 20:16:55 +0530

    some changes

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import BASE_URL from "../utils/config";
 
-const useFetch = (url) => {
+
+const useFetch = (BASE_URL) => {
   const [apiData, setApiData] = useState();
     const [error, setError] = useState()
   
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(url)
+                const response = await fetch(BASE_URL)
                 
                 if(!response.ok){
                     setError('failed to fetch') 
@@ -23,7 +25,7 @@ const useFetch = (url) => {
           }
 
           fetchData();
-    }, [url])
+    }, [BASE_URL])
   
     return {apiData, error}
 }
