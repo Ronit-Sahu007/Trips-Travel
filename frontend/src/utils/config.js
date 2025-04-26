56d458526c1e3826823d916f974793fec886cf0c
RKS File <ronitsahu428@gmail.com>
2025-04-26 22:25:38 +0530

    front url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://trips-travel-server.vercel.app/api";
 // const BASE_URL = "http://localhost:3050/api";
 //
-const BASE_URL="https://trips-travel-backend-mu.vercel.app";
+const BASE_URL="https://trips-travel-backend-mu.vercel.app/api";
 export default BASE_URL;
